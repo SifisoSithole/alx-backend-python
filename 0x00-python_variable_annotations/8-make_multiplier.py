92d0c773e3e5c31702342da39199d640603fb345
SifisoSithole <sitholesifisobrian@gmail.com>
2023-03-30 18:50:03 +0200

    task 8

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+This module contains `make_multiple` function
+"""
+from typing import Callable
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    returns a function that multiplies a float by multiplier
+
+    arg:
+        multiplier (float): multiplier
+
+    return:
+       multiply (function): a function that multiplies a float
+    """
+    def multiply(num: float) -> float:
+        return multiplier * num
+
+    return multiply
