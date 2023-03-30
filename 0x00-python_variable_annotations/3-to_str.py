f84f1d8f87304d3e66c6a795a3464c8240804666
SifisoSithole <sitholesifisobrian@gmail.com>
2023-03-30 13:37:49 +0200

    task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+this module contains the to_str function
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    returns the string represantation of the float
+
+    arg:
+        n (float): float to represent as string
+
+    return:
+        str: string represantation of n
+    """
+    if type(n) is not float:
+        raise TypeError('n must be a float')
+
+    return str(n)
