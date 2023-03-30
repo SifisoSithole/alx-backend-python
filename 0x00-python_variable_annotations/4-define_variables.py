3426a1d876535eb4d4db782c0541008aa840e632
SifisoSithole <sitholesifisobrian@gmail.com>
2023-03-30 13:58:28 +0200

    task 4

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This model contains annotated variables:
+    a (int): an integer with a value of 1
+    pi (float): a float with a value of 3.14
+    i_understand_annotations, a boolean with a value of True
+    school(str): a string with a value of 'Holberton'
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = 'Holberton'
